7fe8c151f631b1fb636402270ef7bf54b81049f4
Juan Renato Noh <jrnq218@gmail.com>
2025-12-04 19:27:31 -0600

    changes

--- iban_registry/app/routers/registries.py
+++ iban_registry/app/routers/registries.py
@@ -12,8 +12,11 @@ async def root():
 @router.post("/upload",summary="Upload the IBAN Registry int txt format provided by swift with the ISO 13616 standard")
 async def upload_registry(file: UploadFile = File(...),service: RegistryService = Depends()):
     
-    service.upload_registry(file)
-
+    try:
+        service.upload_registry(file)
+    except Exception as e:
+        return HTTPException(status_code=400, detail=str(e))
+    
     return {
         "exito": True,
         "mensaje": "Proceso finalizado" 
--- iban_registry/app/services/registries_service.py
+++ iban_registry/app/services/registries_service.py
@@ -2,6 +2,7 @@
 import pandas as pd
 from fastapi import  UploadFile, File
 from ..db.iban_registry_repository import IbanRegistryRepository
+import io
 
 class RegistryService:
 
@@ -13,39 +14,29 @@ class RegistryService:
     Upload the IBAN Registry int txt format provided by swift with the ISO 13616 standard
     """
     def upload_registry(self,file: UploadFile = File(...)): 
+
+        print(f" Leyendo archivo:")
+            
+        # Detectar encoding y leer TXT
+        encodings = ['latin-1', 'cp1252', 'iso-8859-1', 'utf-8']
+        df = None
+            
+        for encoding in encodings:
+            try:
+                df = pd.read_csv(io.StringIO(file.file.read().decode(encoding)), sep='\t', keep_default_na=False)
+                break
+            except UnicodeDecodeError:
+                continue
+
+        if df is None:
+            raise Exception("No se pudo detectar el encoding del archivo")
+
+        # Convertir cada fila a diccionario
+        registros = df.to_dict(orient='records')
+        self.repository.insertar_varios(registros)
+        return True
+            
         
-        ARCHIVO_ENTRADA = 'iban-registry.txt'
-        try:
 
-            with open(ARCHIVO_ENTRADA, "wb") as f:
-                f.write(file.file.read())
 
-            print(f" Leyendo archivo:")
-            
-             # Detectar encoding y leer TXT
-            encodings = ['latin-1', 'cp1252', 'iso-8859-1', 'utf-8']
-            df = None
-            
-            for encoding in encodings:
-                try:
-                    df = pd.read_csv(ARCHIVO_ENTRADA, sep='\t', encoding=encoding, keep_default_na=False)
-                    break
-                except UnicodeDecodeError:
-                    continue
-
-            if df is None:
-                raise Exception("No se pudo detectar el encoding del archivo")
-
-            # Convertir cada fila a diccionario
-            registros = df.to_dict(orient='records')
-            self.repository.insertar_varios(registros)
-            return True
-            
-        except FileNotFoundError:
-            print(f" ERROR: No se encontró el archivo ")
-            print(f"   Verifica que el archivo existe y el nombre es correcto")
-            return False
-            
-        except Exception as e:
-            print(f" ERROR durante la conversión: {str(e)}")
-            return False
+    
